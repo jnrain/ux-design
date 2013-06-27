0c01bbba29c98d74fb87e3fde9ad8c73a13a606c
Wang Xuerui <idontknw.wang@gmail.com>
2013-06-27 15:21:44 +0800

    第二稿，修复非 FF 加载顺序问题，各种调整

--- js/signup.js
+++ js/signup.js
@@ -20,7 +20,20 @@ requirejs(
 
       mod.controller('SignupForm', function($scope) {
         console.log($scope);
+        $scope.debug = true;
+
+        $scope.$watch('personRole', function(to, from) {
+          $scope.isStudent = to == 'stud' || to == 'alumni';
+          $scope.isStaff = to == 'staff';
+          $scope.isInCampus = to == 'stud';
+        });
+        $scope.$watch('personStudRole', function(to, from) {
+          $scope.isUgrad = to == 'ugrad';
+          $scope.isGrad = to == 'grad';
+        });
       });
+
+      angular.bootstrap($('#app-jnrain2-reg'), ['jnrain2-reg']);
     });
 
 
