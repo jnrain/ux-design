9383a0750cabecd25f761b968eb763a2a0ad29a2
Wang Xuerui <idontknw.wang@gmail.com>
2013-06-29 16:28:42 +0800

    分离 shim config

---
+++ js/config.js
@@ -0,0 +1,23 @@
+'use strict';
+
+
+var require = {
+  baseUrl: '//static.jnrain.com/common/js',
+  shim: {
+    'jquery.ui': ['jquery'],
+    'jquery.backstretch': ['jquery'],
+    'garlic': ['jquery'],
+    'parsley': ['jquery'],
+    'angular': {
+      deps: ['jquery'],  // force use of real jQuery
+      exports: 'angular'
+    },
+    'angular-dragdrop': {
+      deps: ['jquery.ui', 'angular'],
+      exports: 'jqyoui'
+    }
+  }
+};
+
+
+// vim:set ai et ts=2 sw=2 sts=2 fenc=utf-8:
