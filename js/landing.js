65c0c206f71762b12d3b68235ceedb14300f1729
Wang Xuerui <idontknw.wang@gmail.com>
2013-07-03 18:59:29 +0800

    指向缩略图一定时间则触发切换；完善

--- js/landing.js
+++ js/landing.js
@@ -23,6 +23,7 @@ requirejs(
             scroll: false
           },
           SWITCH_INTERVAL = 8000,
+          THUMBNAIL_HOVER_SWITCH_INTERVAL = 1500,
           mod = angular.module('jnrain2-signin', ['ngSanitize', 'ngDragDrop']),
           SigninForm = (function($scope) {
             console.log($scope);
@@ -33,42 +34,88 @@ requirejs(
           EventScreen = (function($scope, $timeout) {
             $scope.events = EVENTS;
             $scope.prevEvent = $scope.currEvent = 1;
-            $scope.switchToShot = (function(idx, resetTimer) {
+            $scope.nextImageTimer = $scope.thumbHoverTimer = null;
+
+            $scope.switchToShot = (function(idx, refreshTimer) {
               if (idx == $scope.currEvent) {
                 return;
               }
 
-              $scope.currEvent = idx;
-              resetTimer ? $scope.cancelTimer() : 0;
-              $scope.newTimer();
+              $scope.cancelImageTimer().currEvent = idx;
+              refreshTimer ? $scope.newImageTimer() : 0;
+              return $scope;
             });
+
             $scope.bkgndImage = (function(url) {
               return {
                 "background-image": "url(" + url + ")"
               };
             });
+
             $scope.nextShot = (function() {
               var evt = $scope.currEvent;
 
-              $scope.switchToShot(evt == EVENTS.length - 1 ? 0 : evt + 1, false);
+              $scope.switchToShot(evt == EVENTS.length - 1 ? 0 : evt + 1, true);
+              return $scope;
             });
-            $scope.newTimer = (function() {
-              $scope.nextImageTimer = $timeout($scope.nextShot, SWITCH_INTERVAL);
-            });
-            $scope.cancelTimer = (function() {
+
+            $scope.newImageTimer = (function() {
+              console.log('newImageTimer');
               if ($scope.nextImageTimer === null) {
-                return;
+                $scope.nextImageTimer = $timeout($scope.nextShot, SWITCH_INTERVAL);
+                console.log('image timer created');
               }
 
-              $timeout.cancel($scope.nextImageTimer);
-              $scope.nextImageTimer = null;
+              return $scope;
+            });
+
+            $scope.cancelImageTimer = (function() {
+              console.log('cancelImageTimer');
+              if ($scope.nextImageTimer !== null) {
+                $timeout.cancel($scope.nextImageTimer);
+                $scope.nextImageTimer = null;
+                console.log('image timer cancelled');
+              }
+
+              return $scope;
+            });
+
+            $scope.newHoverTimer = (function(idx) {
+              console.log('newHoverTimer');
+              $scope
+              // 终止任何可能没结束的缩略图悬停定时器
+              .cancelHoverTimer()
+              // 终止可能还在跑的全局定时器
+              .cancelImageTimer();
+
+              $scope.thumbHoverTimer = $timeout(function() {
+                $scope.switchToShot(idx, false);
+                // 这里不要刷新这个悬停定时器，因为没必要
+                // 鼠标指针离开缩略图元素时会触发全局定时器重新开始
+              }, THUMBNAIL_HOVER_SWITCH_INTERVAL);
+
+              return $scope;
+            });
+
+            $scope.cancelHoverTimer = (function(refreshTimer) {
+              console.log('cancelHoverTimer');
+              if ($scope.thumbHoverTimer !== null) {
+                $timeout.cancel($scope.thumbHoverTimer);
+                $scope.thumbHoverTimer = null;
+                console.log('hover timer cancelled');
+              }
+
+              // 需要时重启全局定时器
+              refreshTimer ? $scope.cancelImageTimer().newImageTimer() : 0;
+
+              return $scope;
             });
 
             $scope.$watch('currEvent', function(to, from) {
               (to != from) ? $scope.prevEvent = from : 0;
             });
 
-            $scope.newTimer();
+            $scope.newImageTimer();
 
             console.log($scope);
           });
