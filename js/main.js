7f54ac9d4597f816ae710f7a387296b2ef6f621f
Wang Xuerui <idontknw.wang@gmail.com>
2013-08-12 15:06:50 +0800

    去除 ui.jq 依赖关系, 去掉一些调试信息

--- js/main.js
+++ js/main.js
@@ -2,21 +2,20 @@
 
 
 requirejs(
-    ['angular', 'angular-scrollevents', 'waypoints', 'ui.jq'],
+    ['angular', 'angular-scrollevents', 'waypoints'],
     function(angular) {
-      var mod = angular.module('jnrain2-main', ['ngScrollEvent', 'ui.jq']),
+      var mod = angular.module('jnrain2-main', ['ngScrollEvent']),
           MainPage = (function($scope) {
             $scope.areas = [1, 2, 3, 4];
             $scope.activeArea = 0;
             $scope.topNavHidden = false;
 
             $scope.updateActiveAreaFactory = (function(idx) {
-              console.log('updateActiveAreaFactory: ' + idx);
+              // console.log('updateActiveAreaFactory: ' + idx);
 
               // 返回的是一个 handler
               return (function() {
-                console.log('updateActiveArea: ' + idx);
-                alert('scrolled');
+                // console.log('updateActiveArea: ' + idx);
                 $scope.activeArea = idx;
               });
             });
@@ -47,6 +46,25 @@ requirejs(
           });
 
       MainPage.$inject = ['$scope'];
+      mod.directive('jqWaypoint', function($parse) {
+        return {
+          restrict: 'A',
+          link: function(scope, element, attrs) {
+            var fn = scope.$eval(attrs.jqWaypoint),
+                ctx = attrs.jqWaypointCtx;
+
+            // console.log(element);
+            // console.log('attrs: ' + attrs);
+            // console.log('attrs.jqWaypoint: ' + attrs.jqWaypoint);
+            // console.log('fn: ' + fn);
+            if (typeof ctx !== 'undefined') {
+              element.waypoint(fn, { context: ctx });
+            } else {
+              element.waypoint(fn);
+            }
+          }
+        };
+      });
       mod.controller('MainPage', MainPage);
 
       angular.bootstrap(angular.element('#screen'), ['jnrain2-main']);
